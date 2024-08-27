4716d271d3a88cba1bba011436a34f46315964a0
Andallfor <leozwang2005@gmail.com>
2024-08-27 17:38:31 -0400

    nav bar update and prelim contacts page

--- docs/build/index.js
+++ docs/build/index.js
@@ -4,4 +4,5 @@ relevant commands:
 npx tsc --watch
 npx tailwindcss -i ./src/index.css -o ./docs/build/index.css --watch
 */
-console.log("hello world");
+window.addEventListener('load', () => {
+});
--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ npx tsc --watch
 npx tailwindcss -i ./src/index.css -o ./docs/build/index.css --watch
 */
 
-
-console.log("hello world");
+window.addEventListener('load', () => {
+    
+});
