b6561f35b180e78755c73744e95086efcf8bf14c
Andallfor <leozwang2005@gmail.com>
2024-08-26 23:30:45 -0400

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/*.{html,ts}", "./docs/*.html", "./docs/build/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // https://lospec.com/palette-list/aap-64
+        'black-main': '#060608',
+        'pink-main': '#793a80',
+        'red-main': '#73172d',
+        'blue-main': '#143464'
+      }
+    },
   },
   plugins: [],
 }
