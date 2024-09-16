3b1d59d3fed989723eedf6de294a20314b2b82ad
Andallfor <leozwang2005@gmail.com>
2024-09-16 14:33:39 -0400

    erm

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   root: 'docs/',
-  base: '/docs/',
+  base: '/andallfor-home/',
+  plugins: [react()],
 })
