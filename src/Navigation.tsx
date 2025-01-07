b832c6eaa2e147c37504fb5f7e967e0abf5b1e0d
Andallfor <leozwang2005@gmail.com>
2025-01-07 05:17:32 -0500

    update resume and remove nav images

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -3,12 +3,11 @@ import { useEffect } from 'react';
 interface NavigationButtonConfig {
     name: string,
     highlightColor: string,
-    img: string,
     translateOverride?: string,
-    action: string | (() => void)
+    action: string
 };
 
-function NavigationButton({name, highlightColor, img, translateOverride, action} : NavigationButtonConfig) {
+function NavigationButton({name, highlightColor, translateOverride, action} : NavigationButtonConfig) {
     translateOverride = (translateOverride !== undefined) ? translateOverride : '-translate-y-[100vh] h-[95vh]';
 
     function get(type: '' | '-link' | '-highlight'): HTMLElement {
@@ -22,41 +21,26 @@ function NavigationButton({name, highlightColor, img, translateOverride, action}
         });
     });
 
-    // dont seem to be able to dynamically update a jsx element, and converting to HTML element kinda defeats the purpose of JSX
-    let lc = ['nav-' + name + '-link', "peer w-full text-center pt-8 -translate-y-8 absolute delay-150 cursor-pointer hover:font-black hover:z-50", name.charAt(0).toUpperCase() + name.substring(1)];
-    let link: JSX.Element;
-    if (typeof action === 'string') link = <a id={lc[0]} className={lc[1]} href={action as string}>{lc[2]}</a>;
-    else link = <a id={lc[0]} className={lc[1]} onClick={action}>{lc[2]}</a>;
-
+    let lc = ['nav-' + name + '-link', "peer w-full text-center pt-8 -translate-y-8 absolute delay-150 cursor-pointer", name.charAt(0).toUpperCase() + name.substring(1)];
     return (
         <div id={'nav-' + name} className="w-1/3 relative flex items-center flex-col">
-            {link}
-            <div className={translateOverride.split(' ')[0] + " w-full origin-top relative peer-hover:-translate-y-16 transition-transform z-40 ease-in-out duration-[400ms] justify-center hidden sm:flex"}>
-                <img className={translateOverride.split(' ')[1] + " absolute -z-20 origin-top md:max-w-[50vw] w-screen 2xl:w-full object-cover fade-bottom opacity-80"} src={img}/>
-                <div className={translateOverride.split(' ')[1] + " w-full -z-50 absolute backdrop-blur-sm fade-bottom"}></div>
-            </div>
-            <div id={'nav-' + name + '-highlight'} className='relative peer-hover:translate-y-8 translate-y-4 duration-[350ms] transition-transform pointer-events-none opacity-0 peer-hover:opacity-100 bottom-16 w-full z-50 hidden sm:block'>
+            <a id={lc[0]} className={lc[1]} onClick={() => {
+                const info = document.getElementById(action);
+                if (info !== undefined) info?.scrollIntoView({behavior: 'smooth'});
+            }}>{lc[2]}</a>
+            <div className='absolute w-full h-full bg-red-500'></div>
+            {
+                    <div id={'nav-' + name + '-highlight'} className='relative peer-hover:translate-y-8 translate-y-4 duration-[350ms] transition-transform pointer-events-none opacity-0 peer-hover:opacity-100 bottom-16 w-full z-50 hidden sm:block'>
                 <div className='absolute flex w-full justify-center'>
                 <div className={highlightColor + " absolute md:max-w-[50vw] w-screen 2xl:w-full h-4"}></div>
                 </div>
-            </div>
+            </div>   
+            }
         </div>
     );
 }
 
 export default function Navigation() {
-    function handleNavClick(id: string) {
-        const info = document.getElementById(id);;
-        if (info !== undefined) {
-            info?.scrollIntoView({behavior: 'smooth'});
-            const link = document.getElementById('nav-about-link');
-            // "unhover" the element to hide the dropdown
-            link?.classList.remove('peer');
-            link?.addEventListener('mouseenter', () => link?.classList.add('peer'), {once: true});
-            link?.addEventListener('touchend', () => link?.classList.add('peer'), {once: true});
-        }
-    }
-
     return (
         <div id="navigation-main" className="w-full flex mt-8 sticky top-4 z-[100] select-none">
             <div className="absolute w-full top-4 sm:top-0 h-[calc(100%+1rem+2rem)] -translate-y-8 backdrop-blur-md -z-50"></div>
@@ -74,9 +58,9 @@ export default function Navigation() {
 
             <div className="flex-grow h-0 min-h-full">
                 <div className="flex justify-evenly text-white fira-code-font text-lg md:text-xl mr-4 ml-4">
-                    <NavigationButton name='about' highlightColor="bg-purple-main" img='robotCrop.jpg' translateOverride="-translate-y-[90vh] h-[85vh]" action={() => handleNavClick('information-scroll-anchor')}/>
-                    <NavigationButton name='projects' highlightColor="bg-red-main" img='moonMeshFull.png' action={() => handleNavClick('projects-scroll-anchor')}/>
-                    <NavigationButton name='contact' highlightColor="bg-blue-main" img='barnesHutCrop.png' action={() => handleNavClick('contact-scroll-anchor')}/>
+                    <NavigationButton name='about' highlightColor="bg-purple-main" translateOverride="-translate-y-[90vh] h-[85vh]" action='information-scroll-anchor'/>
+                    <NavigationButton name='projects' highlightColor="bg-red-main" action='projects-scroll-anchor'/>
+                    <NavigationButton name='contact' highlightColor="bg-blue-main" action='contact-scroll-anchor'/>
                 </div>
             </div>
 
